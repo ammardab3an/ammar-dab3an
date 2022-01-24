60a0bf294bb929f5eb863a0e866106ec0e7c8f44
AmmarDab3an <ammar.dabaan99@gmail.com>
2022-01-24 06:58:33 +0200

    backup

---
+++ fcc-frontend-projects/clock/index.js
@@ -0,0 +1,15 @@
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
